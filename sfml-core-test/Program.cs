41f1910245874565254b394ed2daba448cae98ab
suzusime <tamagawajousuiro+git@gmail.com>
2019-04-07 18:34:46 +0900

    文字入力できるように

--- sfml-core-test/Program.cs
+++ sfml-core-test/Program.cs
@@ -12,7 +12,8 @@ namespace sfml_core_test
         {
             RenderWindow window = new RenderWindow(new VideoMode(1024, 768), "きりたん～");
             Font font = new Font("mplus-2p-light.ttf");
-            Text text = new Text("進捗どうですか？", font, 40);
+            string textData = "進捗どうですか？";
+            Text text = new Text(textData, font, 40);
             text.Position = new Vector2f(100, 275);
             RectangleShape shape = new RectangleShape(new Vector2f(400f, 200f));
             shape.FillColor = new Color(125, 140, 100);
@@ -22,6 +23,30 @@ namespace sfml_core_test
             Sprite sprite = new Sprite(texture);
 
             window.Closed += (sender, e) => window.Close();
+            window.TextEntered += (sender, e) =>
+            {
+                if (e.Unicode == "\b")
+                {
+                    if (textData.Length > 0)
+                    {
+                        textData = textData.Substring(0, textData.Length - 1);
+                    }
+                    text = new Text(textData, font, 40);
+                    text.Position = new Vector2f(100, 275);
+                }
+                else if(e.Unicode == "\r")
+                {
+                    textData += "\n";
+                    text = new Text(textData, font, 40);
+                    text.Position = new Vector2f(100, 275);
+                }
+                else
+                {
+                    textData += e.Unicode;
+                    text = new Text(textData, font, 40);
+                    text.Position = new Vector2f(100, 275);
+                }
+            };
             sprite.Position = new Vector2f(350, 20);
 
             while (window.IsOpen)
